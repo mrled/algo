80365fbf1eeecf4c64a1d274e8e392a56225b9bc
Micah R Ledbetter <me@micahrl.com>
2018-05-01 21:31:59 -0500

    Improve the newtroy_nbuuid.py filter module
    - Use argparse for better help
    - Add url_nbuuid()
    - Add dns_nbuuid()

--- filter_plugins/newtroy_nbuuid.py
+++ filter_plugins/newtroy_nbuuid.py
@@ -2,6 +2,7 @@
 
 # A UUID filter that works with namespaces
 
+import argparse
 import sys
 import uuid
 
@@ -17,6 +18,8 @@ class FilterModule(object):
         Per RFC4122, a name-based UUID is generated from a 'name' (any input) and a 'namespace'
         (itself a UUID which uniquely represents the namespace).
 
+        Given the same name and namespace, will always return the same value.
+
         name        Any input
         namespace   A UUID representing the namespace
                     Must be either a valid uuid.UUID object
@@ -27,11 +30,20 @@ class FilterModule(object):
         return uuid.uuid5(namespace, str(name))
 
     @staticmethod
-    def test_nbuuid(name, namespace=None):
-        """Show a simple example result of using the nbuuid() static method
-
-        Intended only for demo/testing purposes.
+    def url_nbuuid(name):
+        """Generate a name-based UUID in the URL namespace
         """
+        return uuid.uuid5(uuid.NAMESPACE_URL, name)
+
+    @staticmethod
+    def dns_nbuuid(name):
+        """Generate a name-based UUID in the DNS namespace
+        """
+        return uuid.uuid5(uuid.NAMESPACE_DNS, name)
+
+    @staticmethod
+    def test_nbuuid(name, namespace=None):
+
         if namespace == None:
             namespace = uuid.uuid4()
             print("Example namespace (randomly generated): {}".format(namespace))
@@ -44,8 +56,39 @@ class FilterModule(object):
     def filters(self):
         return {
             'newtroy_nbuuid': self.nbuuid,
+            'newtroy_url_nbuuid': self.url_nbuuid,
+            'newtroy_dns_nbuuid': self.dns_nbuuid,
         }
 
 
+def main(*args, **kwargs):
+    """Show a simple example result of using the nbuuid() static method
+
+    Intended only for demo/testing purposes.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'name', help="Input for a new name-based UUID")
+    parser.add_argument(
+        'namespace', nargs='?', default=None, type=uuid.UUID,
+        help=(
+            "An optional namespace (which itself must be a UUID) for a name-based UUID. "
+            "If omitted, a randomly generated namespace will be used. "
+            "Note that this program will return the same output UUID "
+            "for the same input name and namespace UUIDs"))
+    parsed = parser.parse_args()
+
+    if not parsed.namespace:
+        namespace = uuid.uuid4()
+        print("Example namespace (randomly generated): {}".format(namespace))
+    else:
+        namespace = parsed.namespace
+        print("Attempting to use passed-in namespace: {}".format(namespace))
+
+    namebased_uuid = FilterModule.nbuuid(parsed.name, namespace)
+    print("UUID for input '{}' in namespace '{}': {}".format(
+        parsed.name, namespace, namebased_uuid))
+
+
 if __name__ == '__main__':
-    FilterModule.test_nbuuid(*sys.argv[1:])
+    main(*sys.argv[1:])
